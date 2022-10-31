b686d3fc24a316783c85ef71e0df8e2fce89a6c7
Tega Okorare <epicboy006@gmail.com>
2022-10-31 15:27:58 +0100

    Updated Work Section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -11,21 +11,21 @@ const workData = [
   },
   {
     id: '2',
-    name: 'Ecommerce',
-    image: 'https://images.unsplash.com/photo-1664575197593-2e7085ef086b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    brand: 'Ikea'
+    name: 'CRM',
+    image: 'https://images.unsplash.com/photo-1667222886295-72267cdc99ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    brand: 'Upwork'
   },
   {
     id: '3',
-    name: 'Ecommerce',
-    image: 'https://images.unsplash.com/photo-1664575197593-2e7085ef086b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    brand: 'Ikea'
+    name: 'Tracker',
+    image: 'https://images.unsplash.com/photo-1666974993470-d1116c9667e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    brand: 'Emergency'
   },
   {
     id: '4',
-    name: 'Ecommerce',
-    image: '',
-    brand: 'Ikea'
+    name: 'Landing Page',
+    image: 'https://images.unsplash.com/photo-1571292388502-66711f403cd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    brand: 'Viralbase'
   }
 ]
 
@@ -34,10 +34,18 @@ const Featured = () => {
     <h1>Works</h1>
     {
       workData.map((item) => (
-        <div className="project-items" key={item.id}>
-          <h1>{item.name}</h1>
-          <img src={item.image} alt="" />
+        <section className="section-container">
+        <div class="container">
+            <figure class="figure">
+                <div class="img-placeholder"><img src={item.image} alt="" /></div>
+                <p class="title">{item.name}</p>
+                <div class="label">
+                    <div class="serial">No.1</div>
+                    <div class="text">Retro</div>
+                </div>
+            </figure>
         </div>
+    </section>
       ))
     }
   </div>;
