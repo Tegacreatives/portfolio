87e77451062949a93b89ecd72b8f4aa1b225238a
Tega Okorare <epicboy006@gmail.com>
2022-11-10 22:04:56 +0100

    Added hamburger menu

--- web/src/components/navbar/Navbar.jsx
+++ web/src/components/navbar/Navbar.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Link } from "gatsby";
+import { RiMenu2Fill } from "react-icons/ri";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="nav" data-scroll-section>
-      <div className="logo">Tega Creatives</div>
-      <ul>
-        <li>
-          <Link to="/projects">Projects.</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact.</Link>
-        </li>
-      </ul>
+    <div data-scroll-section>
+      <div className="nav" data-scroll-section>
+        <div className="logo">Tega Creatives</div>
+        <ul>
+          <li>
+            <Link to="/projects">Projects.</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact.</Link>
+          </li>
+        </ul>
+        {/* mobile toogele navbar */}
+        <div className="menu-open">
+          <RiMenu2Fill size={25} />
+        </div>
+      </div>
+      {/* mobile navbar */}
     </div>
   );
 };
